de81c58cf6e7063deb3c65ee7cb26bd9b1c36160
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2024-03-12 16:27:24 +0530

    Add todo-list code

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,6 +12,35 @@
 */
 
 class Todo {
+  todos = [];
+
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo){
+    if(indexOfTodo < this.todos.length)
+      this.todos.splice(indexOfTodo, 1);
+  }
+
+  update(indexOfTodo, todo){
+    if(indexOfTodo < this.todos.length)
+      this.todos[indexOfTodo] = todo;
+  }
+
+  getAll(){
+    return this.todos;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo < this.todos.length)
+      return this.todos[indexOfTodo];
+    return null;
+  }
+
+  clear(){
+    this.todos = [];
+  }
 
 }
 
